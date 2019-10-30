882dded410effda98a4d72f0a14b93edcac014fe
João Gabriel D. Joaquim <jgdjoaquim@hotmail.com>
2019-10-30 10:23:40 -0300

    Variáveis

---
+++ JavaScript Completo ES6/02-JavaScript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,29 @@
+var nome = "João";
+var idade = 22;
+var possuiFaculdade = false;
+
+// console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+// console.log(totalPreco);
+
+var sobrenome = "Joaquim",
+  cidade = "Caragua";
+
+// console.log(sobrenome, cidade);
+
+// Exercício
+let name = "João Gabriel";
+
+let age = "22";
+
+let favoriteFood;
+favoriteFood = "Pizza";
+
+let val1, val2, val3, val4, val5;
+
+console.log(name, age, favoriteFood);
+console.log(val1, val2, val3, val4, val5);
