4d887c67beb0970cbab6a78531572618ac8dc765
Joao Gabriel D. Joaquim <jgdjoaquim@hotmail.com>
2022-04-20 18:02:43 -0300

    Aulas npx, ambiente e inicio de JSX

---
+++ react-completo/2-React_Iniciantes/07-JSX/appreact/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  const estiloH1 = {
+    color: 'blue',
+    fontSize: '20px',
+    fontFamily: 'Helvetica',
+  };
+
+  return (
+    <div>
+      <h1 style={estiloH1}>Empresa</h1>
+      <p style={{ color: 'green' }}>Sempre aberta</p>
+    </div>
+  );
+};
+
+export default App;
+
