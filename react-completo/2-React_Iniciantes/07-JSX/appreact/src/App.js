f36be2165a4be5842439dfd7680c4ec1cab5cad5
Joao Gabriel D. Joaquim <jgdjoaquim@hotmail.com>
2022-04-28 14:48:22 -0300

    Exercicio JSX
    Exercício JSX

--- react-completo/2-React_Iniciantes/07-JSX/appreact/src/App.js
+++ react-completo/2-React_Iniciantes/07-JSX/appreact/src/App.js
@@ -1,19 +1,56 @@
-import React from 'react';
+// Exercicio JSX
 
-const App = () => {
-  const estiloH1 = {
-    color: 'blue',
-    fontSize: '20px',
-    fontFamily: 'Helvetica',
-  };
+// // Mostre os dados da aplicação, como aprensetado no vídeo
+// // Não utilize CSS externo, use o style para mudar as cores
+// // Se a situação estiver ativa pinte de verde, inativa vermelho
+// // Se o gasto for maior que 10000 mostre uma mensagem
+// const luana = {
+//   cliente: 'Luana',
+//   idade: 27,
+//   compras: [
+//     { nome: 'Notebook', preco: 'R$ 2500' },
+//     { nome: 'Geladeira', preco: 'R$ 3000' },
+//     { nome: 'Smartphone', preco: 'R$ 1500' },
+//   ],
+//   ativa: true,
+// };
 
-  return (
-    <div>
-      <h1 style={estiloH1}>Empresa</h1>
-      <p style={{ color: 'green' }}>Sempre aberta</p>
-    </div>
-  );
-};
+// const mario = {
+//   cliente: 'Mario',
+//   idade: 31,
+//   compras: [
+//     { nome: 'Notebook', preco: 'R$ 2500' },
+//     { nome: 'Geladeira', preco: 'R$ 3000' },
+//     { nome: 'Smartphone', preco: 'R$ 1500' },
+//     { nome: 'Guitarra', preco: 'R$ 3500' },
+//   ],
+//   ativa: false,
+// };
 
-export default App;
+// const App = () => {
+//   const dados = mario;
+
+//   const total = dados.compras
+//     .map((item) => Number(item.preco.replace('R$', '')))
+//     .reduce((a, b) => a + b);
+
+//   return (
+//     <div>
+//       <p>Nome: {dados.cliente}</p>
+//       <p>Idade: {dados.idade}</p>
+//       <p>
+//         Situação:{' '}
+//         <span style={{ color: dados.ativa ? 'green' : 'red ' }}>
+//           {dados.ativa ? 'Ativa' : 'Inativa'}
+//         </span>
+//       </p>
+//       <p>Total gasto: R$ {total}</p>
+//       {total > 10000 && <p>Você está gastando muito</p>}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// Exercicio JSX Arrays
 
